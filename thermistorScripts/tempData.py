d8a9c83a103c46fef14aaa2e2dae9dbd09fd144c
Asahi Jige <ajige@169-231-166-132.wireless.ucsb.edu>
2022-10-01 18:40:03 -0700

    Changes for multiple thermistors

--- thermistorScripts/tempData.py
+++ thermistorScripts/tempData.py
@@ -10,7 +10,8 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 # Function to calculate temperature in celsius given output voltage
-def tempCalc(voltOut):
+def tempCalc(voltList):
+    tempList = []
 
     # Steinhart-Hart Equation Coefficients
     a = 0.00058563
@@ -21,15 +22,18 @@ def tempCalc(voltOut):
     R0 = 10000 # 10k Ohm Resistor used for voltage divider
 
     # Calculate thermistor resistance
-    Rt = (voltOut * R0) / (voltIn - voltOut)
+    for volt in voltList
+        Rt = (volt * R0) / (voltIn - volt)
 
-    # Calculate temperature in Kelvin 
-    tempK = 1 / (a + (b * np.log(Rt)) + (c * np.log(Rt))**3)
+        # Calculate temperature in Kelvin 
+        tempK = 1 / (a + (b * np.log(Rt)) + (c * np.log(Rt))**3)
 
-    # Convert from Kelvin to Celsius
-    tempC = tempK - 273.15
+        # Convert from Kelvin to Celsius
+        tempC = tempK - 273.15
+
+        tempList.append(tempC)
     
-    return tempC
+    return tempList
 
 # Animation initialization script
 def init():
@@ -37,8 +41,8 @@ def init():
 
 # Logging temperature data from DAQ Device and real time plotting
 def animate(frame):
-    voltOut = task.read()
-    temp = tempCalc(voltOut)
+    voltOutList = task.read()
+    tempList = tempCalc(voltOutList)
 
     time = frame * timeInterval
 
@@ -46,25 +50,58 @@ def animate(frame):
         csvWriter = csv.DictWriter(csvFile, fieldnames = fieldNames)
         data = {
             'time (s)': time,
-            'temperature (C)': temp
+            'temp1 (C)': tempList[0], 
+            'temp2 (C)': tempList[1],
+            'temp3 (C)': tempList[2],
+            'temp4 (C)': tempList[3],
+            'temp5 (C)': tempList[4],
+            'temp6 (C)': tempList[5],
+            'temp7 (C)': tempList[6],
+            'temp8 (C)': tempList[7]
         }
         csvWriter.writerow(data)
-        logging.info(f'time (s): {time}; temperature (C): {temp}')
+        logging.info(f'time: {time} \n'
+        f'          temp1, {round(tempList[0], 3)}; temp2, {round(tempList[1], 3)}; temp3, {round(tempList[2], 3)}; temp4, {round(tempList[3], 3)} \n'
+        f'          temp5, {round(tempList[4], 3)}; temp6, {round(tempList[5], 3)}; temp7, {round(tempList[6], 3)}; temp8, {round(tempList[7], 3)}')
 
     data = pd.read_csv('tempData.csv')
     t = data['time (s)']
-    temp = data['temperature (C)']
+    temp1 = np.array(data['temp1 (C)']) 
+    temp2 = np.array(data['temp2 (C)']) + 1
+    temp3 = np.array(data['temp3 (C)']) + 2
+    temp4 = np.array(data['temp4 (C)']) + 3
+    temp5 = np.array(data['temp5 (C)']) + 4
+    temp6 = np.array(data['temp6 (C)']) + 5
+    temp7 = np.array(data['temp7 (C)']) + 6
+    temp8 = np.array(data['temp8 (C)']) + 7
+    
 
-    t = t[-10:]
-    temp = temp[-10:]
+    t = t[-50:]
+    temp1 = temp1[-50:]
+    temp2 = temp2[-50:]
+    temp3 = temp3[-50:]
+    temp4 = temp4[-50:]
+    temp5 = temp5[-50:]
+    temp6 = temp6[-50:]
+    temp7 = temp7[-50:]
+    temp8 = temp8[-50:]
 
     ax.clear()
-    ax.plot(t, temp, marker = 'o')
+    ax.plot(t, temp1, marker = 'o', label = 'Thermistor 1')
+    ax.plot(t, temp2, marker = 'o', label = 'Thermistor 2')
+    ax.plot(t, temp3, marker = 'o', label = 'Thermistor 3')
+    ax.plot(t, temp4, marker = 'o', label = 'Thermistor 4')
+    ax.plot(t, temp5, marker = 'o', label = 'Thermistor 5')
+    ax.plot(t, temp6, marker = 'o', label = 'Thermistor 6')
+    ax.plot(t, temp7, marker = 'o', label = 'Thermistor 7')
+    ax.plot(t, temp8, marker = 'o', label = 'Thermistor 8')
+    ax.legend()
     ax.set_title('Temperature Graph')
     ax.set_ylim(-50, 50)
     ax.set_xlabel('time (s)')
     ax.set_ylabel('temperature (C)')
     ax.grid()
+
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description = 'Thermistor Temp Data Collection')
@@ -93,11 +130,18 @@ else:
 logging.info('Initializing nidaqmx task.')
 task = nidaqmx.Task()
 task.ai_channels.add_ai_voltage_chan('Dev1/ai0', min_val = 0, max_val = 5)
+task.ai_channels.add_ai_voltage_chan('Dev1/ai1', min_val = 0, max_val = 5)
+task.ai_channels.add_ai_voltage_chan('Dev1/ai2', min_val = 0, max_val = 5)
+task.ai_channels.add_ai_voltage_chan('Dev1/ai3', min_val = 0, max_val = 5)
+task.ai_channels.add_ai_voltage_chan('Dev1/ai4', min_val = 0, max_val = 5)
+task.ai_channels.add_ai_voltage_chan('Dev1/ai5', min_val = 0, max_val = 5)
+task.ai_channels.add_ai_voltage_chan('Dev1/ai6', min_val = 0, max_val = 5)
+task.ai_channels.add_ai_voltage_chan('Dev1/ai7', min_val = 0, max_val = 5)
 task.start()
 
 # Open file for data recording
 logging.info('Opening file tempData.csv for data collection.')
-fieldNames = ['time (s)', 'temperature (C)']
+fieldNames = ['time (s)', 'temp1 (C)', 'temp2 (C)', 'temp3 (C)', 'temp4 (C)', 'temp5 (C)', 'temp6 (C)', 'temp7 (C)', 'temp8 (C)']
 with open('tempData.csv', 'w') as csvFile:
     csvWriter = csv.DictWriter(csvFile, fieldnames = fieldNames)
     csvWriter.writeheader()
